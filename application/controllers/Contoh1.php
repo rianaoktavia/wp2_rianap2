417945f86a7ac27f9ed6e669f24cd7a12d292b8d
rianaoktavia <rianaoktavia3333@gmail.com>
2021-10-04 11:14:29 +0700

    WP2_RIANA

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1><br>";
+        echo "Nama Saya <br>
+              Saya tinggal di <br>
+              olahraga yang saya sukai sepak bola & futsal";
+    }
+}
