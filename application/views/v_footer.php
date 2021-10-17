0b6029aa86843c5c5fa40b6d829ae905a732c4a6
rianaoktavia <rianaoktavia3333@gmail.com>
2021-10-17 16:10:47 +0700

    Latihan Pertemuan 4

--- application/views/v_footer.php
+++ application/views/v_footer.php
@@ -1,6 +1,6 @@
-        <footer>
-            <a href=”http://www.RentalBuku.com”>RentalBuku</a>
-        </footer>
-    </div>
+<footer>
+			<a href="http://www.RentalBuku.com">Rental Buku</a>
+		</footer>
+	</div>
 </body>
 </html>
